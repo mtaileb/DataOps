7ce6d4d2c7ae926f296a3fdcde1c21d2fedcf23d
mtaileb <116947757+mtaileb@users.noreply.github.com>
2023-05-08 14:14:50 +0200

    Create main.tf

---
+++ Terraform_avec_Azure/TP2/2.1_Liste_de_maps/main.tf
@@ -0,0 +1,13 @@
+variable listofmaps {
+    type = list(map(string))
+    default = [
+        {
+            name = "ritesh"
+            age = "20"
+        },
+        {
+            name = "avni"
+            age = "10"
+        }
+    ]
+}
