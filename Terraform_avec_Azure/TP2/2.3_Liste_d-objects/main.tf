6c017076643acdec6fb4071763b27a26f2e17f74
mtaileb <116947757+mtaileb@users.noreply.github.com>
2023-05-08 14:17:27 +0200

    Create main.tf

---
+++ Terraform_avec_Azure/TP2/2.3_Liste_d-objects/main.tf
@@ -0,0 +1,22 @@
+variable listofobjects {
+    type = list(object({
+        location = string
+        age = number
+    }
+    ))
+    default = [
+        {
+            location = "hyderabad"
+            age = 20
+        },
+        {
+            location = "kolkata"
+            age = 10
+        },
+        {
+            location = "bangalore"
+            age = 30
+            name = "ritesh"
+        }
+    ]
+}
