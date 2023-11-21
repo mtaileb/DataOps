af1662331a7ba89a91153a6910769f9aa044297f
mtaileb <116947757+mtaileb@users.noreply.github.com>
2023-11-21 21:02:55 +0100

    Update main.tf

--- Terraform_avec_Azure/TP2/8_Multiproviders/main.tf
+++ Terraform_avec_Azure/TP2/8_Multiproviders/main.tf
@@ -2,28 +2,21 @@ terraform {
     required_providers {
         azurerm = {
             source  = "registry.terraform.io/hashicorp/azurerm"
-            version = "~>2.36, ~>2.40"
-            
-        }
-        someotherprovider = {
-            source  = "registry.terraform.io/hashicorp/azurerm"
-            version = "~>2.36, ~>2.40"
-            
+            version = "~>2.36, ~>2.40"        
+        }    
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 4.16"
         }
     }
 }
 
+provider "aws" {
+  region  = "us-west-2"
+}
+
 provider azurerm {
-    features {}
-}
-
-provider someotherprovider  {
-    features {}
+  features {}
 }
 
 
-resource azurerm_resource_group rg {
-    name= "providertestrg"
-    location = "west europe"
-    provider = someotherprovider
-}
