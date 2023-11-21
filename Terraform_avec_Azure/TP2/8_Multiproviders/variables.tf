786c9ee8ac03e817dc1031c6ceab73951cb4d230
mtaileb <116947757+mtaileb@users.noreply.github.com>
2023-11-21 21:06:53 +0100

    Create variables.tf

---
+++ Terraform_avec_Azure/TP2/8_Multiproviders/variables.tf
@@ -0,0 +1,8 @@
+export ARM_SUBSCRIPTION_ID = $subscriptionId
+export ARM_CLIENT_ID = appId
+export ARM_CLIENT_SECRET = password
+export ARM_TENANT_ID = tenant
+
+export AWS_ACCESS_KEY_ID="enter your access key generated earlier"
+export AWS_SECRET_ACCESS_KEY="enter the secret key generated earlier"
+
