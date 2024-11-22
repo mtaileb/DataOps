696e11122de2ccfb43849e780abe8e1f8d59fed7
mtaileb <116947757+mtaileb@users.noreply.github.com>
2024-11-22 14:25:05 +0100

    Create Exemple_test_DAG.py

---
+++ Airflow/Exemple_test_DAG.py
@@ -0,0 +1,7 @@
+# command layout: command subcommand [dag_id] [task_id] [(optional) date]
+
+# testing print_date
+airflow tasks test tutorial print_date 2015-06-01
+
+# testing sleep
+airflow tasks test tutorial sleep 2015-06-01
