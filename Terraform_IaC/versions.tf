ccc87bd1f3c597d58351c2ab2ff5111bcc73fb2f
mtaileb <116947757+mtaileb@users.noreply.github.com>
2024-11-28 15:12:41 +0100

    Update versions.tf

--- Terraform_IaC/versions.tf
+++ Terraform_IaC/versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12" 
+required_providers {
+    azurerm = {
+      version = "~> 2.51.0"
+      source = "hashicorp/azurerm"
+    }
+  }
 }
